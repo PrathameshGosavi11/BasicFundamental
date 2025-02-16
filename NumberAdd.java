babe15d0fb1df54c8f6ad80fe6d322dc68f9e8ec
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-16 19:44:37 +0530

    add some changes

---
+++ NumberAdd.java
@@ -0,0 +1,14 @@
+public class NumberAdd {
+    public static void main(String[] args) {
+        
+        int num1=10; //local variabel
+        int num2=20;
+
+        final int result=num1+num2;
+       
+
+    System.out.println("sum is:"+result);
+  
+
+    }
+}
