515e009e712ac002a4820d61067e65da0f69269f
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-16 19:30:41 +0530

    add basic file

---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
+class Banty
+{
+    public static void main(String[] args) {
+        
+    
+    System.out.println("Banty");
+    }
+    
+}
+class Roshani
+{
+    public static void main(String[] args) {
+        System.out.println("I am roshani");
+    }
+}
